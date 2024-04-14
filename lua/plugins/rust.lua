70030b11a259b30ba1a7d660db2e4c4f8f34ae89
Ethan Howard <ethanh20009@outlook.com>
2024-04-14 22:42:26 +0100

    rust keymap and dashboard day of the week

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,15 @@
+return {
+  "mrcjkb/rustaceanvim",
+  opts = {
+    server = {
+      on_attach = function(_, bufnr)
+        vim.keymap.set("n", "<leader>ca", function()
+          vim.cmd.RustLsp("codeAction")
+        end, { desc = "Code Action", buffer = bufnr })
+        vim.keymap.set("n", "<leader>dr", function()
+          vim.cmd.RustLsp("debuggables")
+        end, { desc = "Rust Debuggables", buffer = bufnr })
+      end,
+    },
+  },
+}
