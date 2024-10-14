019c750bde7b9be50a57022eed66fe9c4cfabf09
Ethan Howard <ethanh20009@outlook.com>
2024-10-14 13:50:54 +0100

    add markdown snippet

---
+++ lua/luasnippets/markdown.lua
@@ -0,0 +1,16 @@
+local luasnip = require("luasnip")
+local s = luasnip.snippet
+local t = luasnip.text_node
+local i = luasnip.insert_node
+luasnip.add_snippets("markdown", {
+  s("matblk", {
+    t({ "$$", "" }),
+    i(1),
+    t({ "", "$$" }),
+  }),
+  s("mat", {
+    t({ "$" }),
+    i(1),
+    t({ "$" }),
+  }),
+})
