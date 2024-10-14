da359bb43bf32b1581aef529e4963a1777a0a368
Ethan Howard <ethanh20009@outlook.com>
2024-10-14 13:54:34 +0100

    cmake shortcuts

---
+++ lua/plugins/cmake.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "cmake-tools.nvim",
+    keys = {
+      { "<leader>Cb", "<cmd>CMakeBuild<CR>", desc = "CMake Build" },
+      { "<leader>Cd", "<cmd>CMakeDebug<CR>", desc = "CMake Run Debug" },
+      { "<leader>Cr", "<cmd>CMakeRun<CR>", desc = "CMake Run" },
+    },
+  },
+}
