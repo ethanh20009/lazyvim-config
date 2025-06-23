ebf7728751aceb506f8e1796961e5d1e39765f80
Ethan Howard <ethanh20009@outlook.com>
2025-06-23 23:28:30 +0100

    Add html snippets to angular without luasnip
    add html snip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,7 +1,7 @@
 return {
-  "L3MON4D3/LuaSnip",
-  init = function()
-    -- extend html snippets for htmlangular
-    require("luasnip").filetype_extend("htmlangular", { "html" })
-  end,
+  -- "L3MON4D3/LuaSnip",
+  -- init = function()
+  --   -- extend html snippets for htmlangular
+  --   require("luasnip").filetype_extend("htmlangular", { "html" })
+  -- end,
 }
