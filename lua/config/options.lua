e5d168f9be0bbd51fbe29feb17351e73d81afbc5
Ethan Howard <ethanh20009@outlook.com>
2025-07-24 11:49:58 +0100

    diffopts, remove local dev db creds and add alpha incr

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.dbs = {
-  { name = "dev", url = "postgres://myuser:mypassword@localhost:5432/corfu-bookings" },
-}
+--
 -- vim.g.lazyvim_python_lsp = "pyright"
-vim.opt.diffopt:append({ "linematch:50" })
+
+vim.opt.diffopt = {
+  "internal",
+  "filler",
+  "closeoff",
+  -- "context:12",
+  "algorithm:patience",
+  "linematch:200",
+  "indent-heuristic",
+  -- "inline:char", -- Probably neovim 12+ needed
+}
+
+vim.opt.nrformats:append("alpha")
