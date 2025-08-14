17000aac860f4c4552bbe4030e745a47e1bb95a0
Ethan Howard <ethanh20009@outlook.com>
2025-08-14 14:36:14 +0100

    obsidian stuff

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -14,7 +14,53 @@ return {
           path = "~/vault/personal",
         },
       },
+      completion = {
+        blink = true,
+      },
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ""
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return suffix
+      end,
       -- see below for full list of options 👇
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      {
+        "saghen/blink.compat",
+      },
+    },
+    opts = {
+      sources = {
+        default = { "obsidian", "obsidian_new", "obsidian_tags" },
+        providers = {
+          obsidian = {
+            name = "obsidian",
+            module = "blink.compat.source",
+          },
+          obsidian_new = {
+            name = "obsidian_new",
+            module = "blink.compat.source",
+          },
+          obsidian_tags = {
+            name = "obsidian_tags",
+            module = "blink.compat.source",
+          },
+        },
+      },
     },
   },
 }
